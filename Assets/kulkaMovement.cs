df9da0f7deed58cc77cb4b896cd659c04617927c
h-okon <hubrr.oko@gmail.com>
2020-03-10 17:32:04 +0100

    Initial Commit

---
+++ Assets/kulkaMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kulkaMovement : MonoBehaviour
+{
+    private int frameCounter;
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    public static float moveSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frameCounter = 0;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public static void setSpeed(int speed)
+    {
+        Debug.Log("Ustawiam predkosc: " + speed);
+        moveSpeed = speed;
+
+    }
+    private void setRotation(Vector2 p1)
+    {
+            frameCounter = 0;
+            float deg = Mathf.Rad2Deg * Mathf.Atan(p1.y / p1.x);
+
+            if (p1.x > 0 && p1.y > 0)      
+            {
+                deg = (deg * -1) + 90;
+            }
+            else if (p1.x > 0 && p1.y < 0)
+            {
+                deg = (deg * -1) + 90;
+            }
+            else if (p1.x < 0 && p1.y < 0)
+            {
+                deg = (deg * -1) + 270;
+            }
+            else                   
+            {
+                deg = (deg * -1) + 270;
+
+            }
+            transform.eulerAngles = Vector3.back * deg;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*   set position   */
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - transform.position).normalized;
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+        setRotation(direction);
+
+    }
+}
---
+++ Assets/swiftPotiononCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swiftPotiononCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider2D m_ObjectCollider;
+
+    void Start()
+    {
+        //Fetch the GameObject's Collider (make sure they have a Collider component)
+        m_ObjectCollider = GetComponent<Collider2D>();
+        //Here the GameObject's Collider is not a trigger
+        m_ObjectCollider.isTrigger = false;
+        //Output whether the Collider is a trigger type Collider or not
+        Debug.Log("Trigger On : " + m_ObjectCollider.isTrigger);
+    }
+
+    void OnCollisionEnter2D()
+    {
+        //GameObject's Collider is now a trigger Collider when the GameObject is clicked. It now acts as a trigger
+        m_ObjectCollider.isTrigger = true;
+        //Output to console the GameObject’s trigger state
+        kulkaMovement.setSpeed(500);
+        Destroy(m_ObjectCollider.gameObject);
+    }
+}
