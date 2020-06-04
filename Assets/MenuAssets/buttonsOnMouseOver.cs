cf2b07980d74df7230754c83b18374b9d2c6b1a0
h-okon <hubrr.oko@gmail.com>
2020-06-04 20:20:19 +0200

    feat: mainMenu: added char spacing when hovering over button

---
+++ Assets/MenuAssets/buttonsOnMouseOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonsOnMouseOver : MonoBehaviour
+{
+    public void changeTextSizeUP()
+    {
+        TextMeshProUGUI txt= GetComponentInChildren<TextMeshProUGUI>();
+        txt.characterSpacing = 20;
+    }
+    public void changeTextSizeDOWN()
+    {
+        TextMeshProUGUI txt = GetComponentInChildren<TextMeshProUGUI>();
+        txt.characterSpacing = 1;
+    }
+}
